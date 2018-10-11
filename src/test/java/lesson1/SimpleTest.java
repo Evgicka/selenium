0b49cd5a8e25f5c3a7cb7c4921ebef8c4e40dbb0
citap <citap.net@yandex.ru>
2018-10-11 19:45:39 +0300

    1

--- src/test/java/lesson1/SimpleTest.java
+++ src/test/java/lesson1/SimpleTest.java
@@ -2,6 +2,7 @@ package lesson1;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.testng.Assert;
@@ -28,6 +29,8 @@ public class SimpleTest {
         driver.findElement(By.cssSelector("[id = Name]")).sendKeys("epam");
         driver.findElement(By.cssSelector("[id = Password]")).sendKeys("1234");
         driver.findElement(By.cssSelector(".login [type = 'submit']")).click();
+
+        //WebElement mainTitle = driver.findElement(By.cssSelector("h3.main-title"));
 
 
 
