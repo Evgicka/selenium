8684d3c1007e7817b443d4d3aae83d97ad5bf37f
citap <citap.net@yandex.ru>
2018-10-18 18:45:34 +0300

    all

--- src/test/java/les4/SimpleTestSelenide.java
+++ src/test/java/les4/SimpleTestSelenide.java
@@ -3,6 +3,8 @@ package les4;
 
 import base.SelenideTestBase;
 import base.TestBase;
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
@@ -13,6 +15,8 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static java.lang.System.setProperty;
@@ -23,26 +27,25 @@ public class SimpleTestSelenide extends SelenideTestBase {
     @Test
     public void SimpleTest1() {
         //1
-        //setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-
-
-        //2
         open("https://epam.github.io/JDI/index.html");
 
 
         //3
-        assertEquals(getWebDriver().getTitle(),"Home Page");
+        assertEquals(getWebDriver().getTitle(), "Home Page");
 
         //4
         $(".profile-photo").click();
         $("[id = Name]").sendKeys("epam");
         $("[id = Password]").sendKeys("1234");
         $(".login [type = 'submit']").click();
-        $$(By.xpath(""));//for collections
         //5
         SelenideElement mainTitle = $("h3.main-title");
-        assertEquals(mainTitle.getText(), "EPAM FRAMEWORK WISHES…");
+        mainTitle.shouldBe(visible);
+        mainTitle.shouldHave(text("EPAM FRAMEWORK WISHES…"));
 
+        //6
+        $$(By.xpath("")).shouldHaveSize(4);//for collections
+        $$(By.xpath("")).shouldBe(CollectionCondition.sizeLessThan(5));//for collections
 
 
 
